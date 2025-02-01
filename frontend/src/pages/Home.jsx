fbc7086a9d291aa1d4b114f25af22865d65c7702
DEVD04 <debdyutidas0@gmail.com>
2025-02-01 07:16:06 +0530

    Navbar,Lead done

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Lead from '../components/Lead'
+import Latest from '../components/Latest'
+
+const Home = () => {
+  return (
+    <div>
+      <Lead />
+      <Latest />
+    </div>
+  )
+}
+
+export default Home
